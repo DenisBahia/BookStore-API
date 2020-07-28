2bab025bbea9cb3e67933a754cd0c425cd261118
DenisBahia <45142971+DenisBahia@users.noreply.github.com>
2020-07-28 11:31:57 +0100

    Add project files.

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+    /// <summary>
+    /// This is a test controller.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ILoggerService _logger;
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/<HomeController>
+        /// <summary>
+        /// Gets values.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            _logger.LogDebug("teste denis bahia ");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<HomeController>/5
+        /// <summary>
+        /// Gets a value.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HomeController>/5
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
